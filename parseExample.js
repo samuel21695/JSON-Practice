d15cc897d2f7a4db5d70c623493f3165faedd78f
samuel21695 <samuel21695@gmail.com>
2023-11-13 16:52:02 +0900

    parsin example

---
+++ parseExample.js
@@ -0,0 +1,9 @@
+const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+
+// JSON 문자열을 JavaScript 객체로 파싱
+const jsonObject = JSON.parse(jsonString);
+
+console.log(jsonString);
+console.log(jsonObject.name); // John
+console.log(jsonObject.age); // 30
+console.log(jsonObject.city); // New York
